445e7d4f6ca823061f068ade0d4de148c0c55d36
Athos Roman <athosgroman@gmail.com>
2021-07-30 21:05:13 -0300

    Ou exclusivo adicionado ao projeto

--- src/OperadorXou.java
+++ src/OperadorXou.java
@@ -14,6 +14,14 @@ public class OperadorXou {
 		
 		//Aqui é preciso criar um if que verifique as duas condições para que o os ventiladores sejam ligados ou não
 		
+		if(umidade>70 ^ temperatura > 26) {
+			System.out.println("Ligando os ventiladores");
+
+		} else {
+			System.out.println("Entrar em contat com um agrônomo");
+		}
+		
+		
 		leitor.close();
 		
 
